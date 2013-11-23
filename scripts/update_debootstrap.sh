8d7ed9912b5ea5b4ef3d914e0c8c2fdc079944ed
panticz <mail@konczalski.de>
2013-11-23 16:21:11 +0100

    Create update_debootstrap.sh

---
+++ scripts/update_debootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# use newest repository
+echo "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" > /etc/apt/sources.list.d/debootstrap.list
+
+# update
+apt-get update -qq
+
+# install debootstrap
+apt-get install -y --force-yes debootstrap
+
+# remove repository
+rm /etc/apt/sources.list.d/debootstrap.list
+apt-get update -qq
