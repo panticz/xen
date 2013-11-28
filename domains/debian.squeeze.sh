42dadff3686e085f6e50bad1504aaef484a6bfdd
panticz <mail@konczalski.de>
2013-11-28 22:45:44 +0100

    Create debian.squeeze.sh

---
+++ domains/debian.squeeze.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# configure domU
+DOMAIN_NAME=squezze
+DOMAIN_MAC=00:09:01:01:aa:bb
+DOMAIN_RAM=2Gb
+DOMAIN_HDD=8Gb
+ 
+# create domU on LVM (for image file use --dir=/root)
+xen-create-image \
+ --hostname=${DOMAIN_NAME} \
+ --dist=squeeze \
+ --lvm=vg0 \
+ --size=${DOMAIN_HDD} \
+ --fs=ext4 \
+ --role=udev \
+ --memory=${DOMAIN_RAM} \
+ --swap=${DOMAIN_RAM} \
+ --dhcp \
+ --mac=${DOMAIN_MAC} \ 
+ --genpass=0 \
+ --password=t00r \
+ --apt_proxy=http://apt-cacher:3142/ \
+ --vcpus $(cat /proc/cpuinfo | grep processor | wc -l)
+ 
+# rename vm config
+mv /etc/xen/${DOMAIN_NAME}.cfg /etc/xen/${DOMAIN_NAME}
+ 
+# OPTIONAL: add to autostart
+ln -s /etc/xen/${DOMAIN_NAME} /etc/xen/auto
+ 
+# start domU
+xm create -c ${DOMAIN_NAME}
+
+# disable pc speaker
+echo 'blacklist snd-pcsp' >> /etc/modprobe.d/blacklist.conf
+
+# check for updates
+if [ $(cat /etc/apt/apt.conf.d/10periodic | grep -c Update-Package-Lists) -eq 0 ]; then
+ echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/10periodic
+fi
+
+# fix FQDN
+sed -i "s|$(hostname) $(hostname)|$(hostname -A)$(hostname)|g" /etc/hosts
+ 
+user: root
+password: t00r
