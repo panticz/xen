5b4c2154fe30c8453b160534204576823a1519ba
panticz <mail@konczalski.de>
2015-06-03 22:22:13 +0200

    create template links

--- scripts/update_debootstrap.sh
+++ scripts/update_debootstrap.sh
@@ -12,3 +12,15 @@ apt-get install -y --force-yes debootstrap
 # remove repository
 rm /etc/apt/sources.list.d/debootstrap.list
 apt-get update -qq
+
+# create template links
+cd /usr/lib/xen-tools/
+[ ! -f  lucid.d ] && ln -s karmic.d lucid.d
+[ ! -f  maverick.d ] && ln -s karmic.d maverick.d
+[ ! -f  natty.d ] && ln -s karmic.d natty.d
+[ ! -f  oneiric.d ] && ln -s karmic.d oneiric.d
+[ ! -f  precise.d ] && ln -s karmic.d precise.d
+[ ! -f  quantal.d ] && ln -s karmic.d quantal.d
+[ ! -f  raring.d ] && ln -s karmic.d raring.d
+[ ! -f  trusty.d ] && ln -s karmic.d trusty.d
+[ ! -f jessie.d ] && ln -s debian.d jessie.d
