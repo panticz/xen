abaeff2304d1646ac731944468d7ad6c75a4ab9a
panticz <mail@konczalski.de>
2016-04-24 21:40:44 +0200

    Update to Ubuntu Xenial

--- scripts/update_debootstrap.sh
+++ scripts/update_debootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # use newest repository
-echo "deb http://archive.ubuntu.com/ubuntu/ vivid main restricted" > /etc/apt/sources.list.d/debootstrap.list
+echo "deb http://archive.ubuntu.com/ubuntu/ xenial main restricted" > /etc/apt/sources.list.d/debootstrap.list
 
 # update
 apt-get update -qq
@@ -23,4 +23,5 @@ cd /usr/lib/xen-tools/
 [ ! -f  quantal.d ] && ln -s karmic.d quantal.d
 [ ! -f  raring.d ] && ln -s karmic.d raring.d
 [ ! -f  trusty.d ] && ln -s karmic.d trusty.d
+[ ! -f  trusty.d ] && ln -s karmic.d xenial.d
 [ ! -f jessie.d ] && ln -s debian.d jessie.d
